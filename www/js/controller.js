a8b714988ea9344e65182fb903a2bb64a223d06e
Matheus <matheus.galdino@hotmail.com>
2017-04-23 17:35:55 -0300

    Comunicação com o servidor realizada - Cadastro

--- www/js/controller.js
+++ www/js/controller.js
@@ -5,3 +5,20 @@ angular.module('nice2meet')
         return $scope.usuario + $scope.senha;
     }
 })
+
+.controller('CadastroCtrl', function($scope, $http){
+
+    $scope.cadastrarTurista = function(usuario){   
+        $http.get('http://projeto-nice2meet-barbaromatrix.c9users.io/api/teste', function(retorno){
+            console.log(retorno);
+        });
+
+        $http({
+            method : "POST",
+            url : "http://projeto-nice2meet-barbaromatrix.c9users.io/api/cadastroTurista",
+            data: usuario
+        }).then(function(retorno){
+            console.log(retorno);
+        });
+    }
+})
