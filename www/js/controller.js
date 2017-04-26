967751026c90fc97a9a028022451fb8d5a9c69c7
luizalex10 <luizalex10@gmail.com>
2017-04-25 23:58:14 -0300

    Bug Arrumado, tela de Mapa feita com menu lateral

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,30 +1,63 @@
 angular.module('nice2meet')
 
-.controller('loginCtrl',function($scope){
-    $scope.login = function(){
-        if($scope.usuario == undefined || $scope.senha == undefined ){
+.controller('loginCtrl', function($scope, $location) {
+    $scope.login = function() {
+        if ($scope.usuario == undefined || $scope.senha == undefined) {
             $scope.erro = "Por favor, preencher login e senha";
-        }
-        else{
-            $scope.erro = ""
+        } else {
+            $scope.erro = "";
+
+            $location.url();
+            $location.url('/home')
         }
     }
 })
 
-.controller('CadastroCtrl', function($scope, $http){
+.controller('CadastroCtrl', function($scope, $http) {
 
-    $scope.cadastrarTurista = function(usuario){   
-        $http.get('http://projeto-nice2meet-barbaromatrix.c9users.io/api/teste', function(retorno){
+    $scope.cadastrarTurista = function(usuario) {
+        $http.get('http://projeto-nice2meet-barbaromatrix.c9users.io/api/teste', function(retorno) {
             console.log(retorno);
         });
 
         $http({
-            method : "POST",
-            url : "http://projeto-nice2meet-barbaromatrix.c9users.io/api/cadastroTurista",
+            method: "POST",
+            url: "http://projeto-nice2meet-barbaromatrix.c9users.io/api/cadastroTurista",
             data: usuario
-        }).then(function(retorno){
+        }).then(function(retorno) {
             console.log(retorno);
         });
     }
 })
 
+
+
+
+
+
+
+.controller('MapCtrl', function($scope, $ionicLoading) {
+    $scope.mapCreated = function(map) {
+        $scope.map = map;
+    };
+
+    $scope.centerOnMe = function() {
+        console.log("Centering");
+        if (!$scope.map) {
+            return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+            content: 'Getting current location...',
+            showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            console.log('Got pos', pos);
+            $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+            $scope.loading.hide();
+        }, function(error) {
+            alert('Unable to get location: ' + error.message);
+        });
+    };
+});
