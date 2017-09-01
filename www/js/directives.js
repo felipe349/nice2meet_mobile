76ece07b1ac5ece2ae8a380979348cf4f7ea97b1
Felipe Formentin <felipe.formentin@ul.com>
2017-09-01 07:26:13 -0300

    Tentando fazer login funcionar

---
+++ www/js/directives.js
@@ -0,0 +1,76 @@
+angular.module('nice2meet')
+
+.directive('map', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            onCreate: '&'
+        },
+        link: function($scope, $element, $attr, $location, $http) {
+            function initialize() {
+                var latlongPraiaGrande = { lat: -24.0295078, lng: -46.4740969 };
+                var locations = [
+                    { "lat": -24.0295078, "lgn": -46.4740969 },
+                    { "lat": -24.0360845, "lgn": -46.4915432 },
+                    { "lat": -23.999416, "lgn": -46.4119249 }
+                ]
+                var myStyle = [
+       {
+         featureType: "administrative",
+         elementType: "labels",
+         stylers: [
+           { visibility: "on" }
+         ]
+       },{
+         featureType: "poi",
+         elementType: "labels",
+         stylers: [
+           { visibility: "off" }
+         ]
+       },{
+         featureType: "water",
+         elementType: "labels",
+         stylers: [
+           { visibility: "off" }
+         ]
+       },{
+         featureType: "road",
+         elementType: "labels",
+         stylers: [
+           { visibility: "on" }
+         ]
+       },{featureType: 'transit',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          }
+
+     ];
+                
+                var map = new google.maps.Map($element[0],  {
+       mapTypeControlOptions: {
+         mapTypeIds: ['mystyle', google.maps.MapTypeId.ROADMAP, google.maps.MapTypeId.TERRAIN]
+       },
+       center: latlongPraiaGrande,
+       zoom: 17,
+       mapTypeId: 'mystyle'
+     });
+            map.mapTypes.set('mystyle', new google.maps.StyledMapType(myStyle, { name: 'My Style' }));
+
+                $scope.onCreate({ map: map });
+
+                // Stop the side bar from dragging when mousedown/tapdown on the map
+                google.maps.event.addDomListener($element[0], 'mousedown', function(e) {
+                    e.preventDefault();
+                    return false;
+                });
+            }
+
+            if (document.readyState === "complete") {
+                initialize();
+            } else {
+                google.maps.event.addDomListener(window, 'load', initialize);
+            }
+        }
+    }
+    
+});
